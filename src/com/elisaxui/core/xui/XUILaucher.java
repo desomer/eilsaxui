f2953b10228de3c19e39e9670aa6be459123e448
Bureau <Bureau@Bureau-PC>
2017-03-18 00:43:07 +0100

    version 1

--- src/com/elisaxui/core/xui/XUILaucher.java
+++ src/com/elisaxui/core/xui/XUILaucher.java
@@ -1,11 +1,41 @@
 package com.elisaxui.core.xui;
 
-public class XUILaucher {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-	public static final void start()
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+public class XUILaucher	extends AbstractHandler
 	{
-		
-		
-	}
+		@Override
+	    public void handle(String target,
+	                       Request baseRequest,
+	                       HttpServletRequest request,
+	                       HttpServletResponse response)
+	        throws IOException, ServletException
+	    {
+			
+			
+			
+	        response.setContentType("text/html;charset=utf-8");
+	        response.setStatus(HttpServletResponse.SC_OK);
+	        baseRequest.setHandled(true);
+	        response.getWriter().println("<h1>Hello World</h1>");
+	    }
+
+	    public static void main(String[] args) throws Exception
+	    {
+	        Server server = new Server(8080);
+	        server.setHandler(new XUILaucher());
+
+	        server.start();
+	        server.join();
+	    }
+
+
 
 }
